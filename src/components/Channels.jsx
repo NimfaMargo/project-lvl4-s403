cb0d465c54474a26b716773d7054cb0401c29b5a
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-20 13:14:34 +0300

    add channels component

---
+++ src/components/Channels.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// // import * as actions from '../actions';
+// // import { filteredTasksSelector } from '../selectors';
+//
+// const mapStateToProps = () => {
+//
+// };
+//
+// const actionCreators = {
+// };
+
+export default class Channels extends React.Component {
+  render() {
+    const { channels } = this.props;
+
+    return (
+        <nav id="sidebar">
+          <div className="sidebar-header">
+              <h4>Channels List</h4>
+          </div>
+          <ul className="list-group components">
+            {channels.map(({ id, name }) => (
+              <li key={id} className="list-group">
+                <a href="#">{name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>);
+  }
+}
+
+// export default connect(mapStateToProps, actionCreators)(Channels);
