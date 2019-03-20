f74d0e5c9125aa5d10d13888f33dcc8b36e362e0
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-20 13:10:33 +0300

    add redux logic and thunk

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/application.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+// import cookies from 'js-cookie';
+import reducers from './reducers';
+import App from './components/App.jsx';
 // import gon from 'gon';
 // import faker from 'faker';
-// import cookies from 'js-cookie';
 // import io from 'socket.io-client';
-
-import Channels from './Channels';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-ReactDOM.render(
-  <Channels channels={window.gon.channels}/>,
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk),
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('chat'),
 );
