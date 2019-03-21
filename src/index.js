4bfae3d7384c2d7ef2cb0f34b0de805a9c4f8581
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-21 13:40:04 +0300

    add cookie and websocket logic

--- src/index.js
+++ src/index.js
@@ -5,25 +5,46 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
-// import cookies from 'js-cookie';
+import Cookies from 'js-cookie';
+import io from 'socket.io-client';
+import gon from 'gon';
+import faker from 'faker';
+import { addMessage } from './actions';
 import reducers from './reducers';
 import App from './components/App.jsx';
-// import gon from 'gon';
-// import faker from 'faker';
-// import io from 'socket.io-client';
+import Context from './utils/context.js';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
+Cookies.set('username', faker.internet.userName());
+
+const preloadedState = gon;
 const store = createStore(
   reducers,
+  preloadedState,
   applyMiddleware(thunk),
 );
 
+const client = io({
+  transports: ['websocket', 'polling', 'flashsocket'],
+});
+
+client.on('connect', () => {
+  client.on('newMessage', ({ data }) => store.dispatch(addMessage(data.attributes)));
+});
+
+
+const username = Cookies.get('username');
+
 render(
   <Provider store={store}>
-    <App />
+    <Context.Provider value={{ username }}>
+      <App />
+    </Context.Provider>
   </Provider>,
   document.getElementById('chat'),
 );
+
+export default Context;
