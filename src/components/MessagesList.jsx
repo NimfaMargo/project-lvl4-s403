6d05408128b77a02a240f7f680034425dc8d6bc1
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-23 15:42:18 +0300

    add style attribute to p element

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -15,7 +15,7 @@ class MessageList extends React.Component {
             {messages.length === 0 ? null : messages.map(({ id, text, username }) => (
              <div key={id} className='row message-base'>
                <div className='col-sm-10 col-md-10 message-text border m-1 '>
-                <p>({username}): {text}</p>
+                <p style={{ wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>({username}): {text}</p>
                </div>
              </div>
             ))}
