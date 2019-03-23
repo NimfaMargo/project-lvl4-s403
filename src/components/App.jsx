e8b5d2ce530ad4673f31a55f5638989aaf926be8
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-23 15:42:49 +0300

    add style attributes for borders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,17 @@ import AddMessage from './AddMessage.jsx';
 const App = () => (
   <div>
     <div className="row justify-content-around" style={{ height: '550px' }}>
-      <div className="sidebar col-sm-12 col-md-3 col-xl-3">
+      <div className="sidebar col-sm-12 col-md-3 col-xl-3" style={{ border: '3px solid #bddfd3' }}>
         <Sidebar/>
       </div>
 
-      <div className="main d-flex flex-sm-column col-sm-12 col-md-9 col-xl-9 mh-100">
+      <div className="main d-flex flex-column col-sm-12 col-md-9 col-xl-9 mh-100" style={{ border: '3px solid #bddfd3' }}>
         <div className="col message-list overflow-auto bg-light align-self-stretch border m-2">
           <MessagesList />
         </div>
       </div>
     </div>
-    <div className="row second-row">
+    <div className="row second-row" style={{ border: '3px solid #bddfd3' }}>
       <div className="col-sm-12 col-md-9 offset-md-3 input">
         <AddMessage />
       </div>
