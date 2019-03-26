4bb6c491f6f6bc0dc83fb12c2d9a402ef6131997
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-26 20:52:20 +0300

    refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@ import { createAction } from 'redux-actions';
 import axios from 'axios';
 import routes from '../utils/routes.js';
 
-export const addMessageRequest = createAction('MESSAGE_ADD_REQUEST');
+// export const addMessageRequest = createAction('MESSAGE_ADD_REQUEST');
 export const addMessageSuccess = createAction('MESSAGE_ADD_SUCCESS');
 export const addMessageFailure = createAction('MESSAGE_ADD_FAILURE');
 
-export const addRequest = (text, currentChannelId, username) => async (dispatch) => {
+export const addMessageRequest = (text, currentChannelId, username) => async (dispatch) => {
   // dispatch(addMessageRequest());
   try {
     const url = routes.messagesUrl(currentChannelId);
