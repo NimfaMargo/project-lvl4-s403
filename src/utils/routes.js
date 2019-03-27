696f8dc2d37f43c420d583f47cad513925c82f15
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-27 21:02:21 +0300

    update description and add rout

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,20 +3,23 @@ const host = ['api', 'v1'];
 // Get List Of Tasks
 // GET /channels
 
-// Get List of Messages for  channelId
-// GET /api/v1/channels/:channelId/messages
+// Get List of Messages for channelId
+// GET /channels/:channelId/messages
 
 // Create new channel
 // POST /channels
 // data: { attributes: { name: "myChannelName" } } }
 
-
 // Create new message
 // POST /channels/:channelId/messages
 // { data: { attributes: { message } } }
 
+// Delete channel by id
+// DELETE /channels/:id
+
 
 export default {
   channelsUrl: () => [...host, 'channels'].join('/'),
+  channelUrl: id => [...host, 'channels', id].join('/'),
   messagesUrl: id => [...host, 'channels', id, 'messages'].join('/'),
 };
