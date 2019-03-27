8b9388baf352cc00f87268e8081bc61354ac583f
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-27 18:26:12 +0300

    add actions for changing channels

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,32 +3,38 @@ import axios from 'axios';
 import routes from '../utils/routes.js';
 
 // export const addMessageRequest = createAction('MESSAGE_ADD_REQUEST');
-export const addMessageSuccess = createAction('MESSAGE_ADD_SUCCESS');
-export const addMessageFailure = createAction('MESSAGE_ADD_FAILURE');
+// export const addMessageSuccess = createAction('MESSAGE_ADD_SUCCESS');
+// export const addMessageFailure = createAction('MESSAGE_ADD_FAILURE');
 
-export const addMessageRequest = (text, currentChannelId, username) => async (dispatch) => {
-  // dispatch(addMessageRequest());
+export const addMessage = createAction('MESSAGE_ADD');
+export const addMessageRequest = (text, currentChannelId, username) => async () => {
   try {
     const url = routes.messagesUrl(currentChannelId);
     await axios.post(url, { data: { attributes: { text, username } } });
-    // dispatch(addMessageSuccess(response));
   } catch (e) {
-    dispatch(addMessageFailure());
     throw new Error(e);
   }
 };
 
-export const addChannelRequest = text => async (dispatch) => {
-  // dispatch(addMessageRequest());
+export const addChannel = createAction('CHANNEL_ADD');
+export const addChannelRequest = text => async () => {
   try {
     const url = routes.channelsUrl();
     await axios.post(url, { data: { attributes: { name: text } } });
-    // dispatch(addMessageSuccess(response));
   } catch (e) {
-    dispatch(addMessageFailure());
     throw new Error(e);
   }
 };
 
-export const addMessage = createAction('MESSAGE_ADD');
-export const addChannel = createAction('Channel_ADD');
+export const getMessagesSuccess = createAction('MESSAGES_GET_SUCCESS');
+export const getMessagesRequest = currentChannelId => async (dispatch) => {
+  try {
+    const url = routes.messagesUrl(currentChannelId);
+    const response = await axios.get(url);
+    dispatch(getMessagesSuccess(response.data));
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+export const changeChannelId = createAction('CHANNEL_ID_CHANGE');
