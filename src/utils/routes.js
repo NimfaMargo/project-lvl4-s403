7f8e9acb2580cbb7fc9ad76539ff6c1fc4819a38
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-27 18:24:44 +0300

    add discription

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,6 +3,14 @@ const host = ['api', 'v1'];
 // Get List Of Tasks
 // GET /channels
 
+// Get List of Messages for  channelId
+// GET /api/v1/channels/:channelId/messages
+
+// Create new channel
+// POST /channels
+// data: { attributes: { name: "myChannelName" } } }
+
+
 // Create new message
 // POST /channels/:channelId/messages
 // { data: { attributes: { message } } }
