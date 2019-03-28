91efadc3e5d79923f2f3c1506761a173b97b7ce3
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-29 01:31:47 +0300

    add scroll logic

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -8,22 +8,39 @@ const mapStateToProps = (state) => {
 
 @connect(mapStateToProps)
 
+
 class MessageList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.messagesEnd = React.createRef();
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom = () => {
+    this.messagesEnd.current.scrollIntoView({ behavior: 'auto' });
+  }
+
   render() {
     const { messages, currentChannelId } = this.props;
-    // console.log(messages, currentChannelId);
-    // console.log(messages.filter(el => console.log(el.channelId)))
     const filteredMessages = messages.filter(el => el.channelId === currentChannelId);
     return (
         <div className='messages w-100 pl-4 pr-4'>
             {filteredMessages.length === 0 ? null
               : filteredMessages.map(({ id, text, username }) => (
-             <div key={id} className='row message-base'>
+             <div key={id} id='newMessage' className='row message-base'>
                <div className='col-sm-12 col-md-12 message-text border rounded m-1 bg-white'>
                 <p style={{ wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>({username}): {text}</p>
                </div>
              </div>
               ))}
+          <div ref={this.messagesEnd} />
         </div>);
   }
 }
