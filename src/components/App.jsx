85ccd83c8527b2f0588864b35749239a5b39f0bf
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-31 23:02:18 +0300

    beautify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,10 @@ import AddMessage from './AddMessage.jsx';
 
 const App = () => (
   <div className="row justify-content-around h-100">
-    <div className="col-sm-12 col-md-3 col-xl-3 sidebar" style={{ background: '#136092d1' }}>
+    <div className="col-sm-12 col-md-4 col-xl-4 sidebar" style={{ background: '#136092d1' }}>
       <Sidebar/>
     </div>
-    <div className="col-sm-12 col-md-9 col-xl-9 main p-0" >
+    <div className="col-sm-12 col-md-8 col-xl-8 main p-0" >
       <div className="container h-100">
         <div className="row message-list overflow-auto bg-light" style={{ height: '550px', background: '#cbd9e663' }}>
           <MessagesList />
