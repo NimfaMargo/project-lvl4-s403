2ac194442fcd8725e2d6117fec3c44286e223714
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-04-03 14:38:31 +0300

    add logic for error handling

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,31 +2,33 @@ import { createAction } from 'redux-actions';
 import axios from 'axios';
 import routes from '../utils/routes.js';
 
-// export const addMessageRequest = createAction('MESSAGE_ADD_REQUEST');
-// export const addMessageSuccess = createAction('MESSAGE_ADD_SUCCESS');
-// export const addMessageFailure = createAction('MESSAGE_ADD_FAILURE');
-
 export const addMessage = createAction('MESSAGE_ADD');
 export const changeChannelId = createAction('CHANNEL_ID_CHANGE');
 export const addChannel = createAction('CHANNEL_ADD');
 export const deleteChannel = createAction('CHANNEL_DELETE');
 export const renameChannel = createAction('CHANNEL_RENAME');
+export const requestFailure = createAction('REQUEST_FAILURE');
+export const requestSuccess = createAction('REQUEST_SUCCESS');
 
-export const addMessageRequest = (text, currentChannelId, username) => async () => {
+export const addMessageRequest = (text, currentChannelId, username) => async (dispatch) => {
   try {
     const url = routes.messagesUrl(currentChannelId);
     await axios.post(url, { data: { attributes: { text, username } } });
-  } catch (e) {
-    throw new Error(e);
+    dispatch(requestSuccess());
+  } catch (error) {
+    dispatch(requestFailure({ error }));
+    throw new Error(error);
   }
 };
 
-export const addChannelRequest = text => async () => {
+export const addChannelRequest = text => async (dispatch) => {
   try {
     const url = routes.channelsUrl();
     await axios.post(url, { data: { attributes: { name: text } } });
-  } catch (e) {
-    throw new Error(e);
+    dispatch(requestSuccess());
+  } catch (error) {
+    dispatch(requestFailure({ error }));
+    throw new Error(error);
   }
 };
 
@@ -35,26 +37,31 @@ export const getMessagesRequest = currentChannelId => async (dispatch) => {
   try {
     const url = routes.messagesUrl(currentChannelId);
     const response = await axios.get(url);
-    dispatch(getMessagesSuccess(response.data));
-  } catch (e) {
-    throw new Error(e);
+    dispatch(getMessagesSuccess(response));
+  } catch (error) {
+    dispatch(requestFailure({ error }));
+    throw new Error(error);
   }
 };
 
-export const deleteChannelRequest = id => async () => {
+export const deleteChannelRequest = id => async (dispatch) => {
   try {
     const url = routes.channelUrl(id);
     await axios.delete(url);
-  } catch (e) {
-    throw new Error(e);
+    dispatch(requestSuccess());
+  } catch (error) {
+    dispatch(requestFailure({ error }));
+    throw new Error(error);
   }
 };
 
-export const updateChannelRequest = (id, newName) => async () => {
+export const updateChannelRequest = (id, newName) => async (dispatch) => {
   try {
     const url = routes.channelUrl(id);
     await axios.patch(url, { data: { attributes: { name: newName } } });
-  } catch (e) {
-    throw new Error(e);
+    dispatch(requestSuccess());
+  } catch (error) {
+    dispatch(requestFailure({ error }));
+    throw new Error(error);
   }
 };
