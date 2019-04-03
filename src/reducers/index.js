e06c130645246c74d29267c367d01a4aecc35fdb
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-04-03 14:39:13 +0300

    add logic for error handle

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,19 @@ import { reducer as formReducer } from 'redux-form';
 import _ from 'lodash';
 import * as actions from '../actions';
 
+const errorMessage = handleActions({
+  [actions.requestFailure](state, { payload: { error } }) {
+    const { message } = error;
+    return message;
+  },
+  [actions.requestSuccess]() {
+    return 'none';
+  },
+  [actions.getMessagesSuccess]() {
+    return 'none';
+  },
+}, 'none');
+
 const channels = handleActions({
   [actions.addChannel](state, channel) {
     return [...state, channel.payload];
@@ -26,8 +39,8 @@ const messages = handleActions({
   [actions.addMessage](state, message) {
     return [...state, message.payload];
   },
-  [actions.getMessagesSuccess](state, { payload }) {
-    return payload.map(el => _.pick(el, 'attributes').attributes);
+  [actions.getMessagesSuccess](state, { payload: { data } }) {
+    return data.map(el => _.pick(el, 'attributes').attributes);
   },
 }, []);
 
@@ -35,5 +48,6 @@ export default combineReducers({
   channels,
   messages,
   currentChannelId,
+  errorMessage,
   form: formReducer,
 });
