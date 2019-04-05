4207c19a7ef9789124dc0f7f50e93f7b226cec75
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-04-05 21:44:51 +0300

    isolate message rendering in separate function

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -27,21 +27,27 @@ class MessageList extends React.Component {
     this.messagesEnd.current.scrollIntoView({ behavior: 'auto' });
   }
 
-  render() {
+  renderMessages = () => {
     const { messages, currentChannelId } = this.props;
     const filteredMessages = messages.filter(el => el.channelId === currentChannelId);
+    if (filteredMessages.length === 0) {
+      return null;
+    }
+    return (filteredMessages.map(({ id, text, username }) => (
+     <div key={id} id='newMessage' className='row message-base'>
+       <div className='col-sm-12 col-md-12 message-text border rounded mb-1 bg-white'>
+        <p className="text-wrap">({username}): {text}</p>
+       </div>
+     </div>))
+    );
+  }
+
+  render() {
     return (
-        <div className='messages w-100 pl-4 pr-4'>
-            {filteredMessages.length === 0 ? null
-              : filteredMessages.map(({ id, text, username }) => (
-             <div key={id} id='newMessage' className='row message-base'>
-               <div className='col-sm-12 col-md-12 message-text border rounded mb-1 bg-white'>
-                <p className="text-wrap">({username}): {text}</p>
-               </div>
-             </div>
-              ))}
-          <div ref={this.messagesEnd} />
-        </div>);
+      <div className='messages w-100 pl-4 pr-4'>
+        {this.renderMessages()}
+        <div ref={this.messagesEnd} />
+      </div>);
   }
 }
 
