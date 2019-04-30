c75adc57f15c868c6bc8c49f8ed91351bc7da1b5
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-05-01 00:06:45 +0300

    add first test

---
+++ __tests__/test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
+import App from '../src/components/App.jsx';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('Store', () => {
+  const store = createStore(reducers);
+
+  const vdom = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const wrapper = mount(vdom);
+  expect(wrapper.render()).toMatchSnapshot();
+});
